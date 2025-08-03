e5f6e8def7d8c73cabc9412037a90e76cae73c8a
Idiarj <idiar16@gmail.com>
2025-08-02 22:47:33 -0400

    minor: added vercel app to the allowed origins in cors config

--- src/configs/cors_config.ts
+++ src/configs/cors_config.ts
@@ -1,5 +1,5 @@
 export const cors_config = {
-    "origin": ["http://localhost:5173", "http://localhost:5174"],
+    "origin": ["http://localhost:5173", "https://frontend-gato-feliz.vercel.app/"],
     "methods": ["GET", "POST", "DELETE", "PUT"],
     "credentials": true
 }
